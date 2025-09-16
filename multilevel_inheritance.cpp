4ef70aa31cf5cb6ebc531b95176c8718a23325dc
ananyakudtarkar <ananyask.03@gmail.com>
2025-09-16 20:22:20 +0530

    Create multilevel_inheritance.cpp

---
+++ multilevel_inheritance.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+public:
+    void eat() {
+        cout << "I can eat." << endl;
+    }
+    void sleep() {
+        cout << "I can sleep." << endl;
+    }
+};
+
+class Mammal : public Animal {
+public:
+    void walk() {
+        cout << "I can walk." << endl;
+    }
+};
+
+
+class Dog : public Mammal {
+public:
+    void bark() {
+        cout << "I can bark." << endl;
+    }
+};
+
+int main() {
+    Dog myDog;
+
+    myDog.eat();
+    myDog.sleep();
+    myDog.walk();
+    myDog.bark();
+
+    return 0;
+}
+
+/*
+OUTPUT
+I can eat.
+I can sleep.
+I can walk.
+I can bark.
+*/
